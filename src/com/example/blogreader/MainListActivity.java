4303f6c14cd7ed0fe8f667f73ddf9272c02116dd
Fer <pher.gm@gmail.com>
2014-04-29 19:48:24 +0200

    changed MainListActivity to extends LisActivity, added ListView to the layout

--- src/com/example/blogreader/MainListActivity.java
+++ src/com/example/blogreader/MainListActivity.java
@@ -1,17 +1,11 @@
 package com.example.blogreader;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
-public class MainListActivity extends ActionBarActivity {
+public class MainListActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
