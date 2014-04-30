8ab61f17a192dc476416e2eac379ed4d1919804e
Fer <pher.gm@gmail.com>
2014-04-30 13:15:46 +0200

    added AsyncTask to get the blog posts in other thread, added INTERNET permission

--- src/com/example/blogreader/MainListActivity.java
+++ src/com/example/blogreader/MainListActivity.java
@@ -1,9 +1,12 @@
 package com.example.blogreader;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.app.ListActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -20,12 +23,9 @@ public class MainListActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
         
-        try{
-            URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count="+NUMBER_OF_POSTS);
-        }
-        catch(MalformedURLException e){
-        	Log.e(TAG,"Exception caught:",e);
-        }
+        GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+        getBlogPostsTask.execute();
+        
     }
 
 
@@ -47,6 +47,32 @@ public class MainListActivity extends ListActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    private class GetBlogPostsTask extends AsyncTask<Object, Void, String> {
+
+		@Override
+		protected String doInBackground(Object... arg0) {
+			int responseCode = -1;
+	        try{
+	            URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count="+NUMBER_OF_POSTS);
+	            HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+	            connection.connect();
+	        }
+	        catch(MalformedURLException e){
+	        	Log.e(TAG,"Exception caught:",e);
+	        }
+	        catch(IOException e){
+	        	Log.e(TAG,"Exception caught:",e);
+	        }
+	        catch(Exception e){
+	        	Log.e(TAG,"Exception caught:",e);
+	        }
+	        
+	        return "Code: " + responseCode;
+		}
+    	
+    	
     }
 
 }
