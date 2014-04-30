4aa10a7862efcce4d4fb7957c092b300ce30746b
Fer <pher.gm@gmail.com>
2014-04-30 12:40:43 +0200

    declared apis url

--- src/com/example/blogreader/MainListActivity.java
+++ src/com/example/blogreader/MainListActivity.java
@@ -1,26 +1,31 @@
 package com.example.blogreader;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.ListActivity;
-import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 
 public class MainListActivity extends ListActivity {
 	
-	protected String[] mAndroidNames;
+	protected String[] mBlogPostTitles;
+	public static final int NUMBER_OF_POSTS = 20;
+	public static final String TAG  = MainListActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
         
-        Resources resources = getResources();
-        mAndroidNames = resources.getStringArray(R.array.android_names);
-        
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
-        setListAdapter(adapter);
+        try{
+            URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count="+NUMBER_OF_POSTS);
+        }
+        catch(MalformedURLException e){
+        	Log.e(TAG,"Exception caught:",e);
+        }
     }
 
 
